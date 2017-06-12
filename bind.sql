97c02301252090870ea96aecee119610b5484f23
vonabarak <bobr@vonabarak.ru>
2017-06-12 13:30:20 +0300

    base structure completly reworked Support for internal and external zones which correspondly returns for request from internal or external network

--- bind.sql
+++ bind.sql
@@ -2,190 +2,159 @@
 -- Database schema
 --
 
-create table soa (
-	id BIGSERIAL PRIMARY KEY,
-  zone VARCHAR(1024) NOT NULL UNIQUE ,          -- zone name
-  ttl INTERVAL not NULL default '20 min',       -- time to live
-  mname VARCHAR(1024) not NULL,                 -- the original or primary source of data for this zone
-  rname VARCHAR(1024) not NULL,                 -- responsible person's email
-  serial CHAR(10) not NULL default              -- number of the original copy of the zone
+CREATE TABLE "record" (
+  id BIGSERIAL PRIMARY KEY,
+  ttl INTERVAL DEFAULT NULL,
+  internal BOOLEAN NOT NULL DEFAULT TRUE,
+  external BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE soa (
+  zone VARCHAR(1024) NOT NULL UNIQUE,           -- zone name
+  ttl INTERVAL NOT NULL DEFAULT '20 min',       -- time to live
+  mname VARCHAR(1024) NOT NULL,                 -- the original or primary source of data for this zone
+  rname VARCHAR(1024) NOT NULL,                 -- responsible person's email
+  serial CHAR(10) NOT NULL DEFAULT              -- number of the original copy of the zone
      regexp_replace(current_date::text, '(....)-(..)-(..)', '\1\2\3') || '01', -- generate serial from date
-  refresh INTERVAL not NULL default '3 hours',  -- time interval before the zone should be refreshed
-  retry INTERVAL not NULL default '1 hour',     -- time interval that should elapse before a failed refresh should be retried
-  expire INTERVAL not NULL default '1 month',   -- time interval that can elapse before the zone is no longer authoritative
-  minimum INTERVAL not NULL default '1 day'     -- minimum TTL field that should be exported with any RR from this zone
-);
+  refresh INTERVAL NOT NULL DEFAULT '3 hours',  -- time interval before the zone should be refreshed
+  retry INTERVAL NOT NULL DEFAULT '1 hour',     -- time interval that should elapse before a failed refresh should be retried
+  expire INTERVAL NOT NULL DEFAULT '1 month',   -- time interval that can elapse before the zone is no longer authoritative
+  minimum INTERVAL NOT NULL DEFAULT '1 day'     -- minimum TTL field that should be exported with any RR from this zone
+) INHERITS(record);
 
-create table a (
-  zone BIGINT not NULL references soa on delete cascade,    -- zone must exists in soa table
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  address inet not NULL
-);
+CREATE TABLE xfr (
+  zone VARCHAR(1024) NOT NULL REFERENCES soa(zone) ON DELETE CASCADE,
+  client VARCHAR(256)
+) INHERITS(record);
 
-create table aaaa (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  address INET not NULL
-);
+CREATE TABLE d_record (
+  host VARCHAR(256) NOT NULL DEFAULT '@',
+  zone VARCHAR(1024) NOT NULL REFERENCES soa(zone) ON DELETE CASCADE
+) INHERITS(record);
 
-create table txt (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  txt_data TEXT not NULL
-);
+CREATE TABLE ns (
+  nsdname VARCHAR(256) NOT NULL
+) INHERITS (d_record);
 
-create table spf (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  txt_data TEXT not NULL
-);
+CREATE TABLE a (
+  address INET NOT NULL
+) INHERITS (d_record);
 
-create table ns (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  nsdname VARCHAR(256) not NULL
-);
+CREATE TABLE aaaa (
+  address INET NOT NULL
+) INHERITS (d_record);
 
-create table mx (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  preference integer not NULL default 10,
-  exchange VARCHAR not NULL
-);
+CREATE TABLE txt (
+  txt_data TEXT NOT NULL
+) INHERITS (d_record);
 
-create table cname (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL,
-  cname VARCHAR(256) not NULL
-);
+CREATE TABLE spf (
+  txt_data TEXT NOT NULL
+) INHERITS (d_record);
 
-create table srv (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  priority integer not NULL,
-  weight integer not NULL,
-  port integer not NULL,
-  target VARCHAR not NULL
-);
+CREATE TABLE mx (
+  priority integer NOT NULL DEFAULT 10,
+  exchange VARCHAR NOT NULL
+) INHERITS (d_record);
 
-create table ptr (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
-  ptrdname VARCHAR(256) not NULL
-);
+CREATE TABLE cname (
+  cname VARCHAR(256) NOT NULL
+) INHERITS (d_record);
 
-create table hinfo (
-  zone BIGINT not NULL references soa on delete cascade,
-  ttl INTERVAL default NULL,
-  host VARCHAR(256) not NULL default '@',
+CREATE TABLE srv (
+  priority integer NOT NULL,
+  weight integer NOT NULL,
+  port integer NOT NULL,
+  target VARCHAR NOT NULL
+) INHERITS (d_record);
+
+CREATE TABLE ptr (
+  ptrdname VARCHAR(256) NOT NULL
+) INHERITS (d_record);
+
+CREATE TABLE hinfo (
   cpu VARCHAR(256),
   os VARCHAR(256),
-  constraint cpu_os_not_null check (coalesce(cpu, os) is not NULL) -- at least one of `cpu', `os' must be not null
+  constraint cpu_os_not_null check (coalesce(cpu, os) is NOT NULL) -- at least one of `cpu', `os' must be not null
+) INHERITS (d_record);
+
+
+-- returns ttl as seconds, returns soa-record's ttl if this record's tll is null
+CREATE OR REPLACE FUNCTION ttl(r_ttl INTERVAL, soa_ttl INTERVAL) RETURNS DOUBLE PRECISION AS $$
+BEGIN
+  IF r_ttl ISNULL THEN
+    RETURN extract('epoch' FROM soa_ttl);
+  ELSE
+    RETURN extract('epoch' FROM r_ttl);
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TYPE d_record_t AS (
+  priority TEXT,
+  data TEXT
 );
 
-create table xfr (
-  zone BIGINT not NULL references soa on delete cascade,
-  client VARCHAR(256)
-);
+-- CREATE OR REPLACE LANGUAGE plpython3u;
+CREATE OR REPLACE FUNCTION data(id BIGINT, rectype TEXT) RETURNS d_record_t as $$
+priority_types = ('mx', 'srv')
+data_types = {
+    'a':     ' host(address) ',
+    'aaaa':  ' host(address) ',
+    'mx':    ' exchange ',
+    'txt':   """ '"' || txt_data || '"' """,
+    'spf':   """ '"' || txt_data || '"' """,
+    'srv':   """ weight::text || ' ' || port::text || ' ' || target::text """,
+    'cname': ' cname ',
+    'ptr':   ' ptrdname ',
+    'hinfo': """ cpu || ' ' || os """
+}
+
+if rectype not in data_types.keys():
+    print('WARNING: Trying to get data from unknown record type "{0}"!'.format(rectype))
+    return None
+
+if id is None:
+    print('WARNING: Trying to access non-existant record of type "{0}"!'.format(rectype))
+    return None
+
+priority = 'priority' if rectype in priority_types else 'NULL'
+
+data = plpy.execute('SELECT {select} as data, {priority} as priority FROM "{table}" WHERE id={id}'.format(
+    select=data_types[rectype],
+    priority=priority,
+    table=rectype,
+    id=id
+), 1)
+
+if data:
+    return data[0]
+else:
+    return {'data': None, 'priority': None}
+$$ LANGUAGE plpython3u;
 
 -- view returning records of all types except `NS' and `SOA'
-create view d_records as
-  select 
-    host,
-    -- if `ttl' in table `a' is not NULL select its value. Select value of `soa.ttl' in other case.
-    case when a.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from a.ttl) end as ttl,
-    'A' as type,
-    NULL as priority,
-    host(address) as data, 
-    soa.zone
-    from a, soa where soa.id = a.zone
-  union
-  select
-    host,
-    case when aaaa.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from aaaa.ttl) end as ttl,
-    'AAAA' as type,
-    NULL as priority,
-    host(address) as data,
-    soa.zone
-    from aaaa, soa where soa.id = aaaa.zone
-  union
-  select
-    host,
-    case when mx.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from mx.ttl) end as ttl,
-    'MX' as type,
-    preference::text as priority,
-    exchange as data,
-    soa.zone
-    from mx, soa where soa.id = mx.zone
-  union
-  select
-    host,
-    case when txt.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from txt.ttl) end as ttl,
-    'TXT' as type,
-    NULL as priority,
-    '"' || txt_data || '"' as data, -- add double quotes to text data cause it can contain whitespaces
-    soa.zone
-    from txt, soa where soa.id = txt.zone
-  union
-  select
-    host,
-    case when spf.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from spf.ttl) end as ttl,
-    'SPF' as type,
-    NULL as priority,
-    '"' || txt_data || '"' as data,
-    soa.zone
-    from spf, soa where soa.id = spf.zone
-  union
-  select
-    host,
-    case when srv.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from srv.ttl) end as ttl,
-    'SRV' as type,
-    priority::text as priority,
-    weight::text || ' ' || port::text || ' ' || target::text as data, -- concatenate fields as DLZ driver do not expect so much elements in tuple returned from postgres
-    soa.zone
-    from srv, soa where soa.id = srv.zone
-  union
-  select 
-    host,
-    case when cname.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from cname.ttl) end as ttl,
-    'CNAME' as type,
-    NULL as priority,
-    cname as data,
-    soa.zone
-    from cname, soa where soa.id = cname.zone
-  union
-  select 
-    host,
-    case when ptr.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from ptr.ttl) end as ttl,
-    'PTR' as type,
-    NULL as priority,
-    ptrdname as data,
-    soa.zone
-    from ptr, soa where soa.id = ptr.zone
-  union
-  select 
-    host,
-    case when hinfo.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from hinfo.ttl) end as ttl,
-    'HINFO' as type,
-    NULL as priority,
-    cpu || ' ' || os as data,
-    soa.zone
-    from hinfo, soa where soa.id = hinfo.zone
-;
+CREATE VIEW d_records AS
+SELECT
+  s.zone                            AS zone,
+  r.host                            AS host,
+  ttl(r.ttl, s.ttl)                 AS ttl,
+  upper(r.tableoid::regclass::TEXT) AS type,
+  d.data                            AS data,
+  d.priority                        AS priority,
+  r.internal                        AS internal,
+  r.external                        AS external
+FROM
+  d_record r,
+  data(r.id, r.tableoid::regclass::TEXT) d,
+  soa s
+WHERE
+  r.zone=s.zone;
 
 -- view returning `NS' and `SOA' record types
-create view ns_records as
-  select
-    case when ns.ttl is NULL then extract('epoch' from soa.ttl) else extract('epoch' from ns.ttl) end as ttl,
+CREATE VIEW ns_records as
+  SELECT
+    ttl(ns.ttl, soa.ttl) AS ttl,
     'NS' as type,
     NULL as priority,
     nsdname as data,
@@ -196,10 +165,12 @@ create view ns_records as
     NULL as expire,
     NULL as minimum,
     soa.zone,
-    host
-    from ns, soa where soa.id = ns.zone
-  union
-  select
+    host,
+    ns.internal,
+    ns.external
+    from ns, soa where soa.zone = ns.zone
+  UNION
+  SELECT
     extract('epoch' from ttl) as ttl,
     'SOA' as type,
     NULL as priority,
@@ -211,13 +182,15 @@ create view ns_records as
     extract('epoch' from expire),
     extract('epoch' from minimum),
     zone,
-    '@' as host
+    '@' as host,
+    internal,
+    external
     from soa
 ;
 
 -- view returning all types of records
-create view all_records as
-  select
+CREATE VIEW all_records as
+  SELECT
     ttl,
     type,
     host as host,
@@ -229,10 +202,12 @@ create view all_records as
     NULL as retry,
     NULL as expire,
     NULL as minimum,
-    zone
+    zone,
+    internal,
+    external
     from d_records
-  union
-  select
+  UNION
+  SELECT
     ttl,
     type,
     host,
@@ -244,7 +219,9 @@ create view all_records as
     retry,
     expire,
     minimum,
-    zone
+    zone,
+    internal,
+    external
     from ns_records
 ;
 
--- drop.sql
+++ drop.sql
@@ -1,4 +1,7 @@
+drop table if exists record_nview cascade;
+drop table if exists nview cascade;
 drop table if exists soa cascade;
+drop table if exists record cascade;
 drop table if exists a cascade;
 drop table if exists aaaa cascade;
 drop table if exists mx cascade;
@@ -10,6 +13,9 @@ drop table if exists xfr cascade;
 drop table if exists cname cascade;
 drop table if exists ptr cascade;
 drop table if exists hinfo cascade;
-drop view if exists dns_records cascade;
+drop view if exists d_records cascade;
 drop view if exists ns_records cascade;
 drop view if exists all_records cascade;
+DROP FUNCTION IF EXISTS data(BIGINT, TEXT);
+DROP FUNCTION IF EXISTS ttl(INTERVAL, INTERVAL);
+drop TYPE if exists d_record_t cascade;
